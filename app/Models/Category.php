48260c65207de460f3af03341c1c52aa70027865
ConvertToInt <tdalgleish7@gmail.com>
2024-07-17 11:16:27 +0100

    Updated relationships in Category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'parent_id'];
+
+    // Category belongs to a parent Category
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    // Category has many subcategories
+    public function subcategories()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    // Category has many products
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
