ee77ebb7d2723ac0a3c466b9c8d3c255e6d8945d
ConvertToInt <tdalgleish7@gmail.com>
2024-07-18 21:10:50 +0100

    Added Product and category to backpack

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
 
 class Product extends Model
 {
+    use CrudTrait;
     use HasFactory;
 
     protected $fillable = ['name', 'slug', 'desc', 'price', 'category_id'];
