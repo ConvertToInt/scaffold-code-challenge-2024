e223384377dab3ceac03bba00e1c0fdee1f37a79
ConvertToInt <tdalgleish7@gmail.com>
2024-07-18 17:37:54 +0100

    Updated category route

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/', [ProductController::class, 'index'])->name('products_index');
-Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('category_show');
+Route::get('/category/{category:slug}', [CategoryController::class, 'show'])->name('category_show');
 Route::get('/search', [ProductController::class, 'search'])->name('search');
