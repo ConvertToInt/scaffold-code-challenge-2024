0bd7194eca6d3c08680adc065d66345cf6d81b57
ConvertToInt <tdalgleish7@gmail.com>
2024-07-18 08:27:19 +0100

    Updated navbar navs to be right aligned

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -48,8 +48,8 @@
           <span class="navbar-toggler-icon"></span>
         </button>
       
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto ">
                 <li class="nav-item active">
                     <a class="nav-link text-white" href="#">Home</a>
                 </li>
